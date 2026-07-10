46719ab26a6a87d8d33eb9a86a30d14a5f89bd76
Ica <herrerajenica21@gmail.com>
2026-07-10 17:50:26 +0800

    used axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import CheckmarkIcon from '../assets/images/icons/checkmark.png';
 import './HomePage.css';
 import {products} from '../../starting-code/data/products.js';
 
 export function HomePage(){
+  axios.get('http://localhost:3000/api/products')
+    .then((response) => {
+      console.log(response.data);
+    })
   return (
     <>
       <title>Ecommerce Project</title>
