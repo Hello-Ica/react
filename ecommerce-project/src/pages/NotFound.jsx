07ab8f3f3446e2f2b856ee40f1644b92cbb4d49d
Ica <herrerajenica21@gmail.com>
2026-07-28 19:04:48 +0800

    lesson 7

--- ecommerce-project/src/pages/NotFound.jsx
+++ ecommerce-project/src/pages/NotFound.jsx
@@ -1,15 +1,18 @@
 import { Header } from '../components/Header'
 import './NotFound.css'
-import { Link } from 'react-router'
 
-export function NotFound({ cart}) {
+
+export function NotFound() {
   return (
     <>
-    <Header cart = {cart}/>
-      <div className="not-found">
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-      </div>
+    <title> 404 Page Not Found</title>
+    <link rel = "icon" type = "image/svg+xml" href = "home-favicon.png" />
+
+    <Header />
+
+    <div className="not-found-message">
+      Page not found
+    </div>
     </>
   )
 }
