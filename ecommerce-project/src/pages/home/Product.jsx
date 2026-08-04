ce79cfb7845a24f9dff969e8bfd4d9821d6c2651
Ica <herrerajenica21@gmail.com>
2026-08-04 18:36:28 +0800

    create add to cart feature

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -5,8 +5,21 @@ import axios from "axios";
 
 export function Product({ product, loadCart}){
   const [quantity, setQuantity] = useState(1);
+  const addToCart = async() => {
+             await axios.post('/api/cart-items', {
+                productId: product.id,
+                quantity
+              });
+              await loadCart();
+            }
+  const selectQuantity = (event) => {
+                const quantitySelected = Number(event.target.value);
+                setQuantity(quantitySelected);
+                
+              }  
+
   return (
-      <div key className="product-container">
+      <div className="product-container">
             <div className="product-image-container">
               <img className="product-image" src={product.image} />
             </div>
@@ -28,11 +41,7 @@ export function Product({ product, loadCart}){
             </div>
 
             <div className="product-quantity-container">
-              <select value = {quantity} onChange={(event) => {
-                const quantitySelected = Number(event.target.value);
-                setQuantity(quantitySelected);
-                console.log(quantitySelected);
-              }}>
+              <select value = {quantity} onChange={selectQuantity} className="product-quantity-select">
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -54,13 +63,7 @@ export function Product({ product, loadCart}){
             </div>
 
             <button className="add-to-cart-button button-primary"
-            onClick={ async() => {
-             await axios.post('/api/cart-items', {
-                productId: product.id,
-                quantity: 1
-              });
-              await loadCart();
-            }}>
+            onClick={ addToCart}>
               Add to Cart
             </button>
           </div>
